234f51cba634b4ebc711d081b8c82f4c8a25388c
Shruti Khandelwal <kh.shruti88@gmail.com>
2024-04-30 15:36:51 -0700

    boilerPlate code

--- server/index.js
+++ server/index.js
@@ -8,4 +8,19 @@ import morgan from 'morgan';
 
 /* CONFIGURATION */
 
-dotenv.config()
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({policy: "cross-origin"}));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cors());
+
+/* ROUTES */
+
+app.use("/client", clientRoutes);
+app.use("/general", generalRoutes);
+app.use("/management", managementRoutes);
+app.use("sales", salesRoutes)
