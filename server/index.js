2d85150d297723f5a2b79218a91f204cbade279c
Shruti Khandelwal <kh.shruti88@gmail.com>
2024-04-30 17:10:34 -0700

    boilerPlate code 2

--- server/index.js
+++ server/index.js
@@ -5,6 +5,10 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import helmet from 'helmet'
 import morgan from 'morgan';
+import clientRoutes from "./routes/client.js";
+import generalRoutes from "./routes/general.js"
+import managementRoutes from "./routes/management.js";
+import salesRoutes from "./routes/sales.js"
 
 /* CONFIGURATION */
 
@@ -23,4 +27,4 @@ app.use(cors());
 app.use("/client", clientRoutes);
 app.use("/general", generalRoutes);
 app.use("/management", managementRoutes);
-app.use("sales", salesRoutes)
+app.use("/sales", salesRoutes)
